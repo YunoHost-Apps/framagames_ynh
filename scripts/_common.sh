cb99659303ebe3a75c430482ccbf63fc2cb6bffc
Félix Piédallu <felix@piedallu.me>
2023-10-04 16:36:56 +0200

    Fix bookworm

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+phpversion="7.3"
 
 # dependencies used by the app
-pkg_dependencies=""
+pkg_dependencies="php$phpversion-fpm"
 
 #=================================================
 # PERSONAL HELPERS
