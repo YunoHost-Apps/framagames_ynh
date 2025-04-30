2a98a9c02dca6d227366349fd47c4f0039b746e8
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-04-30 18:36:09 +0200

    convert to helpers2.1 and update to today’s commit to fix build

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-nodejs_version=12
-
-#=================================================
-# PERSONAL HELPERS
-#=================================================
+nodejs_version=16
 
 _configure_framagames() {
-    ynh_replace_string --target_file="$install_dir/sources/src/data/main.yml" \
-        --match_string="canonical: .*" \
-        --replace_string="canonical: https://$domain$path"
+    ynh_replace --file="$install_dir/sources/src/data/main.yml" \
+        --match="canonical: .*" \
+        --replace="canonical: https://$domain$path"
 
-    ynh_replace_string --target_file="$install_dir/sources/src/data/main.yml" \
-        --match_string="framanav: true" \
-        --replace_string="framanav: false"
+    ynh_replace --file="$install_dir/sources/src/data/main.yml" \
+        --match="framanav: true" \
+        --replace="framanav: false"
 
     # %/ is here to prevent double slash when path is /
-    ynh_replace_string --target_file="$install_dir/sources/config/env.js" \
-        --match_string="  basepath:.*" \
-        --replace_string="  basepath: '${path%/}/', wrong_path: false"
+    ynh_replace --file="$install_dir/sources/config/env.js" \
+        --match="  basepath:.*" \
+        --replace="  basepath: '${path%/}/', wrong_path: false"
 }
 
 _build_install_framagames() {
     pushd "$install_dir/sources"
-        ynh_use_nodejs
-        ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" "$ynh_npm" run commons
-        ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" "$ynh_npm" run prod
+        ynh_hide_warnings ynh_exec_as_app npm run commons
+        ynh_hide_warnings ynh_exec_as_app npm run prod
     popd
 
-    ynh_secure_remove --file="$install_dir/www"
+    ynh_safe_rm "$install_dir/www"
     mv -f "$install_dir/sources/dist" "$install_dir/www"
     cp -r "$install_dir/sources/games/"* "$install_dir/www"
     if [[ "$path" != "/" ]]; then
@@ -40,11 +35,3 @@ _build_install_framagames() {
     fi
     chown -R "$app:www-data" "$install_dir"
 }
-
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
-
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
